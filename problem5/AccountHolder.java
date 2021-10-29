1f86c48c533a87bf42d700abbd6fff5285e1cef8
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-10-28 18:25:09 -0700

    Initial commit

---
+++ problem5/AccountHolder.java
@@ -0,0 +1,12 @@
+public class AccountHolder{
+    protected final int ID;
+    protected final String address;
+
+    public AccountHolder(int ID, String address){
+        this.ID = ID;
+        this.address = address;
+    }
+
+    private static int idCounter = 0;
+    public static int nextID(){ return this.idCounter++; }
+}
